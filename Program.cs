f77ca0422e45ac163ea4396b6c180c595f8fe90b
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-10-19 15:57:01 -0400

    completed exercise 2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var faveSubject = Console.ReadLine().ToLower();
+
+            switch (faveSubject)
+            {
+                case "math":
+                    Console.WriteLine("I love math!");
+                    break;
+                case "english":
+                    Console.WriteLine("english is awesome!");
+                    break;
+                case "science":
+                    Console.WriteLine("science is great!");
+                    break;
+                case "history":
+                    Console.WriteLine("ugh... history...");
+                    break;
+                case "music":
+                    Console.WriteLine("music is my favorite too!");
+                    break;
+                default: Console.WriteLine("hmmm... I'm not sure about that one.");
+                    break;
+            }
+
+
         }
     }
 }
